d092cf32a417f287f83a779b177dabe41862f774
Andron96 <drandarik2@gmail.com>
2023-12-12 13:12:41 +0200

    3

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
 <?php
-
+$value = readline("Введите значение от 1 до 6 и Вы узнаете кто Вы: ");
+switch ($value) {
+    case 1:
+        echo "Вы Пёрень";
+        break;
+    case 2:
+        echo "Вы Пацик на моцике";
+        break;
+    case 3:
+        echo "Вы Джон";
+        break;
+    case 4:
+        echo "Вы Кот";
+        break;
+    case 5:
+        echo "Вы Крот";
+        break;
+    case 6:
+        echo "Вы Сына";
+        break;
+    default:
+        echo "Зачем вы ввели неправильное значение?";
+}
